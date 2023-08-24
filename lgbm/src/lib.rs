3a26e1c1e468382f62e6d6b682869ac4bb860c4e
frozenlib <frozenlib@users.noreply.github.com>
2023-08-24 21:24:16 +0900

    Add use Mat.

--- lgbm/src/lib.rs
+++ lgbm/src/lib.rs
@@ -11,7 +11,7 @@ pub(crate) mod utils;
 pub use booster::*;
 pub use dataset::*;
 pub use error::*;
-pub use mat::MatBuf;
+pub use mat::{Mat, MatBuf};
 pub use parameters::Parameters;
 
 #[cfg(doctest)]
